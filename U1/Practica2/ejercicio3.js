67062da4d367881f0864f0a18b19934426cc67f8
Celia de la Morena Mora <cemorenam8@gmail.com>
2017-09-22 16:39:39 +0200

    ejercicio

---
+++ U1/Practica2/ejercicio3.js
@@ -0,0 +1,3 @@
+console.info("se trata de realizar mensajes de salida por consola");
+console.warn("la profesora dijo literalmente: 'JavaScript es muy \'divertido\' y muy \'facil\' de aprender.");
+console.error("Tienes que habilitar JavaScript en tu navegador");
